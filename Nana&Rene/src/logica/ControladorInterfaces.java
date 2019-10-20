718bab98153b85cddede500a4e034720e806641a
René Suazo <renecsc@gmail.com>
2019-10-20 02:47:55 -0300

    Comentarios en enviaCorreo y controladorInterfaces

--- Nana&Rene/src/logica/ControladorInterfaces.java
+++ Nana&Rene/src/logica/ControladorInterfaces.java
@@ -31,6 +31,7 @@ import InterfazGrafica.VistaHistorialCliente;
  * @author elias
  */
 public class ControladorInterfaces {
+    //Se declaran todas las vistas que utiliza el sistema
     private static VistaPaginaPrincipal principal;
     private static VistaNuevoPedido nuevoPedido;
     private static VistaPedidos gestionaPedido;
@@ -50,7 +51,11 @@ public class ControladorInterfaces {
     private static ReporteProductos reporteProductos;
     private static Almacen almacen;
     private static GestionExcel ge;
-        
+    /**
+     * Constructor
+     * @param almacen
+     * @param ge 
+     */    
     public ControladorInterfaces(Almacen almacen, GestionExcel ge){
         this.almacen = almacen;
         this.ge = ge;
@@ -82,7 +87,9 @@ public class ControladorInterfaces {
         this.reporteProductos = new ReporteProductos();
         
     }
-    
+    /**
+     * Inicia la interfaz con la vista principal
+     */
     public static void iniciarInterfaz(){
         ControladorInterfaces.principal.setLocationRelativeTo(null);
         ControladorInterfaces.principal.setVisible(true);
@@ -90,38 +97,58 @@ public class ControladorInterfaces {
         ControladorInterfaces.principal.setGe(ge);
         ControladorInterfaces.principal.repaint();
     }
-    
+    /**
+     * Muestra u oculta la vista principal
+     * @param b 
+     */
     public static void mostrarPrincipal(boolean b){
         ControladorInterfaces.principal.setVisible(b);
         ControladorInterfaces.principal.setAlmacen(almacen);
         ControladorInterfaces.principal.setGe(ge);
         ControladorInterfaces.principal.repaint();
     }
-    
+    /**
+     * Muestra u oculta la vista de Nuevo pedido
+     * @param b 
+     */
     public static void mostrarNuevoPedido(boolean b){
         ControladorInterfaces.nuevoPedido.setVisible(b);
         ControladorInterfaces.nuevoPedido.setAlmacen(almacen);
         ControladorInterfaces.nuevoPedido.repaint();
     }
-    
+    /**
+     * Muestra u oculta la vista de gestionar pedido
+     * @param b 
+     */
     public static void mostrarGestionaPedido(boolean b){
         ControladorInterfaces.gestionaPedido.setVisible(b);
         ControladorInterfaces.gestionaPedido.setAlmacen(almacen);
         ControladorInterfaces.gestionaPedido.repaint();
     }
-    
+    /**
+     * Muestra u oculta la vista de productos
+     * @param b 
+     */
     public static void mostrarProductos(boolean b){
         ControladorInterfaces.productos.setVisible(b);
         ControladorInterfaces.productos.setAlmacen(almacen);
         ControladorInterfaces.productos.repaint();
     }
-    
+    /**
+     * Muestra u oculta la vista de nuevo producto
+     * @param b 
+     */
     public static void mostrarNuevoProducto(boolean b){
         ControladorInterfaces.nuevoProducto.setVisible(b);
         ControladorInterfaces.nuevoProducto.setAlmacen(almacen);
         ControladorInterfaces.nuevoProducto.repaint();
     }
-    
+    /**
+     * Muestra u oculta la vista de editar producto
+     * @param b
+     * @param producto
+     * @param fila 
+     */
     public static void mostrarEditarProducto(boolean b, Producto producto, int fila){
         ControladorInterfaces.editarProducto.setVisible(b);
         ControladorInterfaces.editarProducto.setProducto(producto);
@@ -129,13 +156,21 @@ public class ControladorInterfaces {
         ControladorInterfaces.editarProducto.setAlmacen(almacen);
         ControladorInterfaces.editarProducto.repaint();
     }
-    
+    /**
+     * Muestra u oculta la vista de materias primas
+     * @param b 
+     */
     public static void mostrarMateriasPrimas(boolean b){
         ControladorInterfaces.materiasPrimas.setVisible(b);
         ControladorInterfaces.materiasPrimas.setAlmacen(almacen);
         ControladorInterfaces.materiasPrimas.repaint();
     }
-    
+    /**
+     * Muestra u oculta la vista de editar materia prima
+     * @param b
+     * @param materia
+     * @param fila 
+     */
     public static void mostrarEditarMateriaPrima(boolean b, MateriaPrima materia,
             int fila){
         ControladorInterfaces.editarMateriaPrima.setVisible(b);
@@ -143,53 +178,85 @@ public class ControladorInterfaces {
         ControladorInterfaces.editarMateriaPrima.setFila(fila);
         ControladorInterfaces.editarMateriaPrima.repaint();
     }
-    
+    /**
+     * Muestra u oculta la vista de registrar materia prima
+     * @param b 
+     */
     public static void mostrarRegistrarMateriaPrima(boolean b){
         ControladorInterfaces.registarMateriaPrima.setVisible(b);
         ControladorInterfaces.registarMateriaPrima.setAlmacen(almacen);
         ControladorInterfaces.registarMateriaPrima.repaint();
     }
-    
+    /**
+     * Muestra u oculta la vista de detalle pedido
+     * @param b
+     * @param pedido 
+     */
     public static void mostrarDetallePedido(boolean b, Pedido pedido){
         ControladorInterfaces.habilitarGestionaPedido(false);
         ControladorInterfaces.vistaDetallePedido.setVisible(b);
         ControladorInterfaces.vistaDetallePedido.inicializaTabla(pedido);
         ControladorInterfaces.vistaDetallePedido.repaint();
     }
-    
+    /**
+     * Muestra u oculta la vista de historial del cliente
+     * @param b
+     * @param cliente 
+     */
     public static void mostrarHistorialCliente(boolean b, Cliente cliente){
         ControladorInterfaces.habilitarClientes(false);
         ControladorInterfaces.vistaHistorialCliente.setVisible(b);
         ControladorInterfaces.vistaHistorialCliente.inicializaTabla(cliente);
         ControladorInterfaces.vistaHistorialCliente.repaint();
     }
-    
+    /**
+     * Muestra u oculta la vista de detalle producto
+     * @param b
+     * @param producto 
+     */
     public static void mostrarDetalleProducto(boolean b, Producto producto){
         ControladorInterfaces.habilitarGestionaProducto(false);
         ControladorInterfaces.vistaDetalleProducto.setVisible(b);
         ControladorInterfaces.vistaDetalleProducto.inicializaTabla(producto);
         ControladorInterfaces.vistaDetalleProducto.repaint();
     }
-    
+    /**
+     * Habilita clientes
+     * @param b 
+     */
     public static void habilitarClientes(boolean b){
         ControladorInterfaces.clientes.enable(b);
     }
-    
+    /**
+     * Habilita gestionaPedido
+     * @param b 
+     */
     public static void habilitarGestionaPedido(boolean b){
         ControladorInterfaces.gestionaPedido.enable(b);
     }
-    
+    /**
+     * Habilita gestionaProducto
+     * @param b 
+     */
     public static void habilitarGestionaProducto(boolean b){
         ControladorInterfaces.productos.enable(b);
     }
 
-    
+    /**
+     * Muestra u oculta la vista de clientes
+     * @param b 
+     */
     public static void mostrarClientes(boolean b){
         ControladorInterfaces.clientes.setVisible(b);
         ControladorInterfaces.clientes.setAlmacen(almacen);
         ControladorInterfaces.clientes.repaint();
     }
-    
+    /**
+     * Muestra u oculta la vista de editar cliente
+     * @param b
+     * @param cliente
+     * @param fila 
+     */
     public static void mostrarEditarCliente(boolean b, Cliente cliente, int fila){
         ControladorInterfaces.editarCliente.setVisible(b);
         ControladorInterfaces.editarCliente.setCliente(cliente);
@@ -197,19 +264,28 @@ public class ControladorInterfaces {
         ControladorInterfaces.editarCliente.setAlmacen(almacen);
         ControladorInterfaces.editarCliente.repaint();
     }
-    
+    /**
+     * Muestra u oculta la vista de nuevo cliente
+     * @param b 
+     */
     public static void mostrarNuevoCliente(boolean b){
         ControladorInterfaces.nuevoCliente.setVisible(b);
         ControladorInterfaces.nuevoCliente.setAlmacen(almacen);
         ControladorInterfaces.nuevoCliente.repaint();
     }
-    
+    /**
+     * Muestra u oculta la vista de reportes de ventas
+     * @param b 
+     */
     public static void mostrarReporteVentas(boolean b){
         ControladorInterfaces.reporteVentas.setVisible(b);
         //ControladorInterfaces.reporteVentas.setAlmacen(almacen);
         ControladorInterfaces.reporteVentas.repaint();
     }
-    
+    /**
+     * Muestra u oculta la vista de reporte de productos
+     * @param b 
+     */
     public static void mostrarReporteProductos(boolean b){
         ControladorInterfaces.reporteProductos.setVisible(b);
         //ControladorInterfaces.reporteProductos.setAlmacen(almacen);
